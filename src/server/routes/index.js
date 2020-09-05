c850b7a95abef9f88ef51a56e73b27ef5e7beddf
fukunagaa <k.k.0627ku@gmail.com>
2020-09-05 11:48:13 +0900

    ルートパスをオプションとする

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const options = {
+  root: path.join(__dirname, "..", "..", "..", "public"),
+};
+
 /* GET home page. */
 router.get("/*", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "..", "..", "..", "public", "index.html"));
+  res.sendFile("index.html", options);
 });
 
 module.exports = router;
