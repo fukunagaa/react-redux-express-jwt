507d42b5d7addb320324c91ca54321a593850288
fukunagaa <k.k.0627ku@gmail.com>
2020-09-06 18:02:18 +0900

    NavをLayout.jsの上に表示

--- src/client/components/Layout.js
+++ src/client/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Layout = ({ location, children }) => {
   console.log(location);
   return (
     <div>
+      <NavBar location={location} />
       <div>{children}</div>
     </div>
   );
