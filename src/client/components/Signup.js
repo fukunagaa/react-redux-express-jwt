ed37a57594b82796bca3849be5b8a453e2763ea3
fukunagaa <k.k.0627ku@gmail.com>
2020-09-08 10:17:34 +0900

    signupコンポーネント useStateの追加

--- src/client/components/Signup.js
+++ src/client/components/Signup.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import className from "classnames";
 import Icon from "../assets/create-signup.svg";
 import commonStyles from "../Stylesheets/common.module.scss";
 import signupStyles from "../Stylesheets/signup.module.scss";
 import placeholderStyles from "../stylesheets/inputPlaceholder.module.scss";
+import { signupRequest } from "../redux/actions";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const dispach = useDispatch();
+  const signup = () => {
+    if (password1 != password2) {
+      setErrorMessage("** 入力されたパスワードが一致しません。 **");
+    } else {
+      dispach(signupRequest({ email, userName, password: password1, firstName, lastName }));
+    }
+  };
+  const errorElement =
+    errorMessage.length == 0 ? (
+      <span></span>
+    ) : (
+      <div className={commonStyles.textAlignCenter}>
+        <span className={commonStyles.errorMessage}>{errorMessage}</span>
+      </div>
+    );
   return (
     <div className={commonStyles.mainContainer}>
       <div className={commonStyles.contentsContainer}>
@@ -19,6 +44,7 @@ const Signup = () => {
           <h3 className={commonStyles.textAlignCenter}>Sign up</h3>
         </div>
         <div className={commonStyles.formContainer}>
+          {errorElement}
           <div className={commonStyles.inputFlexRowContainer}>
             <div className={commonStyles.inputSmallArea}>
               <label className={placeholderStyles.labelInputPlaceholder}>
@@ -29,6 +55,8 @@ const Signup = () => {
                   placeholder="&nbsp;"
                   required
                   className={placeholderStyles.labelInputPlaceholder}
+                  value={firstName}
+                  onChange={(event) => setFirstName(event.target.value)}
                 />
                 <span className={placeholderStyles.labelPlaceholder}>First Name *</span>
               </label>
@@ -42,6 +70,8 @@ const Signup = () => {
                   placeholder="&nbsp;"
                   required
                   className={placeholderStyles.labelInputPlaceholder}
+                  value={lastName}
+                  onChange={(event) => setLastName(event.target.value)}
                 />
                 <span className={placeholderStyles.labelPlaceholder}>Last Name *</span>
               </label>
@@ -56,6 +86,8 @@ const Signup = () => {
                 placeholder="&nbsp;"
                 required
                 className={placeholderStyles.labelInputPlaceholder}
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
               <span className={placeholderStyles.labelPlaceholder}>Email Address *</span>
             </label>
@@ -64,13 +96,45 @@ const Signup = () => {
             <label className={placeholderStyles.labelInputPlaceholder}>
               <input
                 type="text"
+                name="userName"
+                id="userName"
+                placeholder="&nbsp;"
+                required
+                className={placeholderStyles.labelInputPlaceholder}
+                value={userName}
+                onChange={(event) => setUserName(event.target.value)}
+              />
+              <span className={placeholderStyles.labelPlaceholder}>User Name*</span>
+            </label>
+          </div>
+          <div className={commonStyles.inputBigArea}>
+            <label className={placeholderStyles.labelInputPlaceholder}>
+              <input
+                type="password"
                 name="password"
                 id="password"
                 placeholder="&nbsp;"
                 required
                 className={placeholderStyles.labelInputPlaceholder}
+                value={password1}
+                onChange={(event) => setPassword1(event.target.value)}
               />
               <span className={placeholderStyles.labelPlaceholder}>Password *</span>
+            </label>
+          </div>
+          <div className={commonStyles.inputBigArea}>
+            <label className={placeholderStyles.labelInputPlaceholder}>
+              <input
+                type="password"
+                name="password2"
+                id="password2"
+                placeholder="&nbsp;"
+                required
+                className={placeholderStyles.labelInputPlaceholder}
+                value={password2}
+                onChange={(event) => setPassword2(event.target.value)}
+              />
+              <span className={placeholderStyles.labelPlaceholder}>one more Password *</span>
             </label>
           </div>
           <div className={commonStyles.checkboxContainer}>
