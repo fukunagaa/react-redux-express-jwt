bad6599a956b9b973e1177566c709cf7932d86b2
fukunagaa <k.k.0627ku@gmail.com>
2020-09-09 22:49:48 +0900

    PostArticle画面の追加

--- src/client/App.js
+++ src/client/App.js
@@ -5,6 +5,7 @@ import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
+import PostArticle from "./components/PostArticle";
 
 const App = () => {
   const { isLogin } = useSelector((state) => state.user);
@@ -13,13 +14,16 @@ const App = () => {
     <Router>
       <Layout>
         <Route exact path="/">
-          {isLogin ? <Home /> : <Redirect to={"/login"} />}
+          <Home />
         </Route>
         <Route exact path="/login">
           {isLogin ? <Redirect to={"/"} /> : <Login />}
         </Route>
         <Route exact path="/signup">
           {isLogin ? <Redirect to={"/"} /> : <Signup />}
+        </Route>
+        <Route exact path="/postArticle">
+          <PostArticle />
         </Route>
       </Layout>
     </Router>
--- src/client/components/PostArticle.js
+++ src/client/components/PostArticle.js
@@ -1,20 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
-import { Redirect } from "react-router";
+import Cropper from "react-easy-crop";
+import postArticleStyles from "../stylesheets/postArticle.module.scss";
+import getCroppedImg from "./CropImage";
+import "regenerator-runtime/runtime";
+import catImg from "../assets/cat.jpg";
 
 const Home = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [rotation, setRotation] = useState(0);
+  const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+  const [croppedImage, setCroppedImage] = useState(null);
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+  const showCroppedImage = useCallback(async () => {
+    try {
+      const cropped = await getCroppedImg(catImg, croppedAreaPixels, rotation);
+      console.log("donee", { cropped });
+      setCroppedImage(cropped);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [croppedAreaPixels, rotation]);
   const dispach = useDispatch();
   return (
     <div>
       <h1>PostArticle Screen</h1>
       <div>
-        <form method="post" action="/postArticle">
-          <input type="text" />
-          <input type="text" />
-          <input type="file" accept="image/*" />
+        <form method="post" action="/postArticle" className={postArticleStyles.mainContainer}>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="file" accept="image/*" />
+          </div>
+          <div className={postArticleStyles.cropContainer}>
+            <Cropper
+              image={catImg}
+              crop={crop}
+              rotation={rotation}
+              zoom={zoom}
+              aspect={1 / 1}
+              onCropChange={setCrop}
+              onRotationChange={setRotation}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+            />
+          </div>
+          <div className={postArticleStyles.cropContainer}>
+            <img className={postArticleStyles.image} src={croppedImage}></img>
+          </div>
           <button type="submit">投稿する</button>
         </form>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="3"
+            step="0.1"
+            value={zoom}
+            onChange={(event) => setZoom(event.target.value)}
+          />
+          <label>Zoom</label>
+        </div>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="360"
+            step="1"
+            value={rotation}
+            onChange={(event) => setRotation(event.target.value)}
+          />
+          <label>Rotation</label>
+        </div>
+        <button onClick={() => showCroppedImage()}>Result</button>
       </div>
     </div>
   );
