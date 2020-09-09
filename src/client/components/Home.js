3f36988f7119672bc7a64c4961fa8544feb3adae
fukunagaa <k.k.0627ku@gmail.com>
2020-09-09 18:40:03 +0900

    Home画面の修正 セッションをみにいく

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { changeLoginStatus } from "../redux/actions";
+import { Redirect } from "react-router";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Screen</h1>
-    </div>
-  );
+  const [redirectTo, setRedirectTo] = useState(false);
+  const dispach = useDispatch();
+  useEffect(() => {
+    axios
+      .get("/login/ok")
+      .then((res) => {
+        console.log(res.status);
+      })
+      .catch(() => {
+        console.log("catch!!");
+        dispach(changeLoginStatus({ isLogin: false, isAdmin: false, isUser: false }));
+        setRedirectTo(true);
+      });
+  });
+  if (redirectTo) {
+    return <Redirect to="/login" />;
+  } else {
+    return (
+      <div>
+        <h1>Home Screen</h1>
+      </div>
+    );
+  }
 };
 
 export default Home;
