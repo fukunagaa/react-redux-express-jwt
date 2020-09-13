85914e8fcb772759a3a92b619348007707176364
fukunagaa <k.k.0627ku@gmail.com>
2020-09-13 15:33:19 +0900

    GetArticle画面 実装

--- src/client/pages/GetArticle.js
+++ src/client/pages/GetArticle.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import postArticleStyles from "../stylesheets/postArticle.module.scss";
+import getArticleStyles from "../stylesheets/getArticle.module.scss";
 import commonStyles from "../Stylesheets/common.module.scss";
 import Icon from "../assets/add_task.svg";
+import { changeLoginStatus } from "../redux/actions";
+import axios from "axios";
 
 const GetArticle = () => {
+  const [articles, setArticles] = useState([]);
+  console.log(articles);
+  const dispach = useDispatch();
+  useEffect(() => {
+    if (articles.length == 0) {
+      axios
+        .post("/article/fetchAll")
+        .then((res) => {
+          setArticles(res.data.articles);
+        })
+        .catch(() => {
+          dispach(changeLoginStatus({ isLogin: false, isAdmin: false, isUser: false }));
+        });
+    }
+  });
+  const articlesElement = articles.map((article, index) => {
+    console.log(article);
+    return (
+      <div className={getArticleStyles.articleContainer} key={index}>
+        <span>{article.title}</span>
+        <span>{article.contents}</span>
+        <img className={getArticleStyles.image} src={article.image} />
+      </div>
+    );
+  });
+  console.log(articlesElement);
   return (
     <div className={commonStyles.mainContainer}>
       <div className={commonStyles.contentsContainer}>
@@ -18,6 +48,14 @@ const GetArticle = () => {
               </div>
             </div>
             <h3 className={commonStyles.textAlignCenter}>閲覧</h3>
+          </div>
+          <div className={getArticleStyles.articlesContainer}>
+            <div className={`${getArticleStyles.articleContainer} ${getArticleStyles.headerContainer}`} key="header">
+              <span>タイトル</span>
+              <span>内容</span>
+              <span>イメージ</span>
+            </div>
+            {articlesElement}
           </div>
         </div>
       </div>
