d3cb50ad551c97601904843c8603e697350bd36a
fukunagaa <k.k.0627ku@gmail.com>
2020-09-14 20:50:21 +0900

    不要な変数を削除

--- src/client/pages/PostArticle.js
+++ src/client/pages/PostArticle.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import postArticleStyles from "../stylesheets/postArticle.module.scss";
 import commonStyles from "../Stylesheets/common.module.scss";
