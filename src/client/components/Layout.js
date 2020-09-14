700185539999443c241be70088ceb4b63afc82cf
fukunagaa <k.k.0627ku@gmail.com>
2020-09-14 20:49:01 +0900

    Layoutに反映

--- src/client/components/Layout.js
+++ src/client/components/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import NavBar from "./NavBar";
+import { SocketProvider } from "./SocketProvider";
 
 const Layout = ({ location, children }) => {
   console.log(location);
   return (
     <div>
       <NavBar location={location} />
-      <div>{children}</div>
+      <SocketProvider>
+        <div>{children}</div>
+      </SocketProvider>
     </div>
   );
 };
