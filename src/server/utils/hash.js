4b89529fd85aecf14a75e32c31d0993dcfbad399
fukunagaa <k.k.0627ku@gmail.com>
2020-09-15 21:25:47 +0900

    コメントの追加

--- src/server/utils/hash.js
+++ src/server/utils/hash.js
@@ -1,10 +1,16 @@
 const bcrypt = require("bcrypt");
 
 module.exports = {
+  /**
+   * パスワードを入力し、エンコードして返却する
+   */
   encode: (password) => {
     const hashedPassword = bcrypt.hashSync(password, 10);
     return hashedPassword;
   },
+  /**
+   * 入力されたパスワードとエンコードしたパスワードを比較する
+   */
   compare: async (password, hashedPassword) => {
     return await bcrypt.compare(password, hashedPassword);
   },
