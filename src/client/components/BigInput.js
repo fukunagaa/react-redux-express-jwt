2963612f94343971cda07ac98f941848597cca6d
fukunagaa <k.k.0627ku@gmail.com>
2020-09-17 00:10:46 +0900

    JavaDoc追加

--- src/client/components/BigInput.js
+++ src/client/components/BigInput.js
@@ -3,6 +3,14 @@ import React from "react";
 import commonStyles from "../Stylesheets/common.module.scss";
 import placeholderStyles from "../stylesheets/inputPlaceholder.module.scss";
 
+/**
+ * ビッグサイズのinputエリアのコンポーネント
+ * @param {inputのtype} type
+ * @param {idとnameの名前} name
+ * @param {inputのvalue} value
+ * @param {valueのset関数} setValue
+ * @param {placeholderに表示する名前} displayName
+ */
 const BigInput = ({ type, name, value, setValue, displayName }) => {
   return (
     <div className={commonStyles.inputBigArea}>
